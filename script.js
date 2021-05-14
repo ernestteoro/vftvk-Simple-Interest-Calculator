a269fa894e7cd62cdb9623216d06e55f8cdaec88
ernestteoro <fteoro6@gmail.com>
2021-05-14 23:34:13 +0000

    Final Project Hands-On Lab

--- script.js
+++ script.js
@@ -15,19 +15,24 @@ function principalOnChangeEvent() {
 function compute() {
 
     amount = document.getElementById("principal").value;
-    rateValue = document.getElementById("interestRate").value
-    year = document.getElementById("years").value
+    if (amount <= 0) {
+        alert("Please inter a positive number")
+        document.getElementById("principal").value = ""
+        document.getElementById("principal").focus();
+    } else {
+        rateValue = document.getElementById("interestRate").value
+        year = document.getElementById("years").value
 
-    /// Calculating the interest amount
-    var interestAmount = (parseInt(amount) * (parseInt(rateValue) / 100)) * parseInt(year)
+        // Calculating the interest amount
+        var interestAmount = (parseInt(amount) * (parseInt(rateValue) / 100)) * parseInt(year)
 
-    // getting the current date
-    const moonLanding = new Date();
-    // Displaying computed data
-    document.getElementById("result").innerHTML = interestAmount
-    document.getElementById("depositAmount").innerHTML = document.getElementById("principal").value;
-    document.getElementById("depositRate").innerHTML = document.getElementById("interestRate").value
-    document.getElementById("numYears").innerHTML = (parseInt(moonLanding.getFullYear()) + parseInt(year))
-
+        // getting the current date
+        const moonLanding = new Date();
+        // Displaying computed data
+        document.getElementById("result").innerHTML = interestAmount
+        document.getElementById("depositAmount").innerHTML = document.getElementById("principal").value;
+        document.getElementById("depositRate").innerHTML = document.getElementById("interestRate").value
+        document.getElementById("numYears").innerHTML = (parseInt(moonLanding.getFullYear()) + parseInt(year))
+    }
 
 }
